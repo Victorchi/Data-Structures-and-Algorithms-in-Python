0323b79d148403a6483d462163968619707a4ee2
victor <qwe312231@163.com>
2018-01-15 17:00:03 +0800

    [update]  map demo

--- map.py
+++ map.py
@@ -1,2 +1,20 @@
-#coding:utf8
-#Author:Victor Chi
+# coding:utf8
+# Author:Victor Chi
+
+_list = [1, 2, 3, 4, 5, 6, 7]
+_list2 = []
+
+
+# for i in _list:
+#     a = i*i
+#     _list2.append(a)
+
+# print(_list2)
+
+# def fun(i):
+#     return i * i
+
+
+a = map(lambda x:x*x,_list)
+
+print(list(a))
